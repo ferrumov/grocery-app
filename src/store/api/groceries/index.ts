efb8d71cf82ec984bc96a08764e7e9b23d52a200
Alexander <alexander@sweetcode.pro>
2024-09-18 19:45:06 +0300

    implement optimistic updates

--- src/store/api/groceries/index.ts
+++ src/store/api/groceries/index.ts
@@ -2,7 +2,7 @@ import { Platform } from 'react-native';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IGroceryItem } from 'types';
 
-const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:3000/' : 'http://localhost:3000/';
 
 const tagTypes = ['Groceries'];
 
@@ -11,20 +11,35 @@ type EditItemPayload = Pick<IGroceryItem, 'id' | 'name' | 'category' | 'count'>;
 type CompleteItemPayload = Pick<IGroceryItem, 'id' | 'completed'>;
 type RemoveItemPayload = Pick<IGroceryItem, 'id'>;
 
-// TODO: Renmae to groceriesApi
 export const groceriesApi = createApi({
   tagTypes,
   reducerPath: 'groceriesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  keepUnusedDataFor: 0,
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    fetchFn(input, init) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(fetch(input, init));
+        }, 2000);
+      });
+    },
+  }),
   endpoints: (builder) => ({
     list: builder.query<IGroceryItem[], any>({
-      query: () => '/list',
-      providesTags: tagTypes,
+      query: () => 'list',
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Groceries' as const, id })),
+              { type: 'Groceries', id: 'LIST' },
+            ]
+          : [{ type: 'Groceries', id: 'LIST' }],
     }),
 
     createItem: builder.mutation<any, CreateItemPayload>({
       query: (data) => ({
-        url: `/list`,
+        url: `list`,
         method: 'POST',
         body: {
           ...data,
@@ -33,33 +48,93 @@ export const groceriesApi = createApi({
           createdAt: Date.now(),
         },
       }),
-      invalidatesTags: tagTypes,
+      async onQueryStarted({ ...patch }, api) {
+        const patchResult = api.dispatch(
+          groceriesApi.util.updateQueryData('list', undefined, (draft) => {
+            // @ts-ignore
+            draft.push({ ...patch, completed: false, id: `${Date.now()}`, createdAt: Date.now() });
+          }),
+        );
+
+        try {
+          await api.queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: [{ type: 'Groceries', id: 'LIST' }],
     }),
 
     updateItem: builder.mutation<any, EditItemPayload>({
       query: ({ id, ...data }) => ({
-        body: data,
+        body: { id, ...data },
         method: 'PATCH',
-        url: `/list/${id}`,
+        url: `list/${id}`,
       }),
-      invalidatesTags: tagTypes,
+      async onQueryStarted({ id, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          groceriesApi.util.updateQueryData('list', undefined, (draft) => {
+            const updateItem = draft.find((item) => item.id === id);
+            if (updateItem) {
+              Object.assign(updateItem, patch);
+            }
+          }),
+        );
+
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: (_, __, { id }) => [{ type: 'Groceries', id }],
     }),
 
     completeItem: builder.mutation<any, CompleteItemPayload>({
       query: ({ id, completed }) => ({
         method: 'PATCH',
-        url: `/list/${id}`,
+        url: `list/${id}`,
         body: { completed },
       }),
-      invalidatesTags: tagTypes,
+      async onQueryStarted({ id, ...patch }, api) {
+        const patchResult = api.dispatch(
+          groceriesApi.util.updateQueryData('list', undefined, (draft) => {
+            const updateItem = draft.find((item) => item.id === id);
+            if (updateItem) {
+              Object.assign(updateItem, patch);
+            }
+          }),
+        );
+
+        try {
+          await api.queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: (_, __, { id }) => [{ type: 'Groceries', id }],
     }),
 
     removeItem: builder.mutation<any, RemoveItemPayload>({
       query: ({ id }) => ({
         method: 'DELETE',
-        url: `/list/${id}`,
+        url: `list/${id}`,
       }),
-      invalidatesTags: tagTypes,
+      async onQueryStarted({ id }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          groceriesApi.util.updateQueryData('list', undefined, (draft) => {
+            const index = draft.findIndex((item) => item.id === id);
+            draft.splice(index, 1);
+          }),
+        );
+
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+      invalidatesTags: (_, __, { id }) => [{ type: 'Groceries', id }],
     }),
   }),
 });
